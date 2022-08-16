c8a99b02392654ff25f97dc0e0ff646c7662f81b
Artem Degtjarenko <degtjarenko.artem2508@gmail.com>
2022-08-16 14:11:49 +0200

    commit

--- db/mysql/init.sql
+++ db/mysql/init.sql
@@ -1,18 +1,17 @@
 DROP SCHEMA IF EXISTS task;
 CREATE SCHEMA task;
 
-CREATE TABLE task.media(
-    id INT primary key auto_increment not null,
-    filename text not null,
-    url text not null
-);
-
 CREATE TABLE task.product(
     id INT primary key auto_increment not null,
     name Text NULL,
-	description TEXT,
-    picture_id INT,
-    FOREIGN KEY (picture_id) REFERENCES media(id) ON DELETE CASCADE ON UPDATE CASCADE
+	description TEXT
+);
+CREATE TABLE task.media(
+    id INT primary key auto_increment not null,
+    filename text not null,
+    url text not null,
+	product_id INT,
+	FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE task.price(
